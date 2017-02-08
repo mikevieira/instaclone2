616cba0e4843c0c5b5e89b977da65c0a9f296c43
Mike Vieira <mikevieira@hotmail.com>
2017-02-08 15:41:49 +0000

    Retry on instaclone

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,91 @@
+class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+
+def index
+  @grams = Gram.all
+end
+
+def destroy
+  @gram = Gram.find_by_id(params[:id])
+  if @gram.blank?
+    return render_not_found
+  end
+  if @gram.user != current_user
+    return render_forbidden
+  end  
+  @gram.destroy
+  redirect_to root_path
+
+end
+
+def update
+  @gram =Gram.find_by_id(params[:id])
+  if @gram.blank?
+    return render_not_found
+  end
+
+  if @gram.user != current_user
+    return render_forbidden
+  end  
+
+  @gram.update_attributes(gram_params)
+  
+  if @gram.valid?
+    redirect_to root_path
+  else
+   return render :edit, status: :unprocessable_entity 
+  end
+
+end
+
+def edit
+  @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+     return render_not_found 
+    end
+    if @gram.user != current_user
+      return render_forbidden
+    end
+end
+
+def show
+  @gram =Gram.find_by_id(params[:id])
+  if @gram.blank?
+    return render_not_found
+  end
+end
+
+
+
+def new
+  @gram = Gram.new
+end 
+
+
+def create
+  @gram = current_user.grams.create(gram_params)
+  if @gram.valid?
+     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+
+end
+
+
+private
+
+def gram_params
+  params.require(:gram).permit(:message, :picture)
+end
+
+def render_not_found
+  render plain: 'Not Found :(', status: :not_found
+end
+
+def render_forbidden
+   render plain: 'Forbidden :(', status: :forbidden
+end
+
+end
